dc93a9fd6682f663be4fd5cd996219464a47980d
Abdullah Ghaus <abdullahghaus0440@gmail.com>
2022-12-31 16:47:01 +0500

    [BE BP] - Database Connected

---
+++ Backend/config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+  mongoose.set("strictQuery", false);
+  mongoose
+    .connect(process.env.DB_URL)
+    .then((data) => {
+      console.log(`mongodb connected with server ${data.connection.host}`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = connectDatabase;
