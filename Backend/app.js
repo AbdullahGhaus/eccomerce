b75c89b080cbaeab884962be73b795a0ab31076e
Abdullah Ghaus <abdullahghaus0440@gmail.com>
2023-01-02 13:24:42 +0500

    Database Conencteda

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const app = express()
 app.use(express.json())
-
+const errorMiddleware = require("./middleware/error")
 // Route imports
 const product = require("./routes/productRoute")
 
 app.use("/api/v1",product)
 
+//Error MiddleWare
+app.use(errorMiddleware)
 module.exports = app;
--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,3 +1,76 @@
-exports.getAllProducts = (req, res) => {
-  res.status(200).json({ message: "Route is working fine" });
+const Product = require("../models/productModel");
+const ErrorHandler = require("../utils/errorHandler");
+
+//Create Product -- Admin
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
+
+//Get All Products
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+  res.status(200).json({
+    success: true,
+    products,
+  });
+};
+
+//Update Product -- Admin
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new ErrorHandler("Product Not Found",404))
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+//Delete Product -- Admin
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product Not Found",
+    });
+  }
+
+  await product.remove();
+
+  res.status(200).json({
+    success: true,
+    message: "Product Deleted Successfully",
+  });
+};
+
+//Get Product Details
+exports.getProductDetails = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product Not Found",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    product
+  });
 };
