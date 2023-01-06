cb8913d0e2108459a8b3b37939b174c71a7024cb
abdullahghausliqteq <abdullah.ghaus@liqteq.com>
2023-01-06 13:43:21 +0500

    AR

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,13 +1,11 @@
 const express = require("express");
-const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController");
-const { isAuthenticated, authorizeRoles } = require("../middleware/auth");
 
 const products = require('./productRoute')
 const users = require('./userRoute')
 const router = express.Router();
 
 router.use('/products', products)
-router.use('/users', users)
+router.use('/user', users)
 
 
 module.exports = router;
